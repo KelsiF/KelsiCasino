38d9912062dc7fd051916672a7e1c66343409eef
Zodys <166365980+Zodys@users.noreply.github.com>
2024-04-22 01:30:19 +0300

    Add files via upload
    fixes

--- src/main/java/kelsi/kelsicasino/listeners/guiListener.java
+++ src/main/java/kelsi/kelsicasino/listeners/guiListener.java
@@ -10,79 +10,38 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Random;
 
 public class guiListener implements Listener {
 
-    int number = 0;
-    int slot = 0;
+    ConcurrentHashMap<Player, Integer> playerSlotNumberMap = new ConcurrentHashMap<>();
     Random random = new Random();
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
-        int randomNumber = random.nextInt(100 - 1)+1;
+        int randomNumber = random.nextInt(100) + 1;
 
         if (player.hasMetadata("EnterNumber")) {
+            int number = Integer.parseInt(event.getMessage());
+            event.setCancelled(true);
+
+            Integer slot = playerSlotNumberMap.get(player);
+            if (slot != null && slot >= 1 && slot <= 7) {
+                double[] multipliers = {1.1, 1.5, 1.75, 2, 3, 3.5, 5};
+                int[] chances = {95, 80, 75, 50, 25, 10, 1};
+
+                if (randomNumber <= chances[slot - 1]) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number * multipliers[slot - 1]));
+                } else {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco take " + player.getName() + " " + number);
+                }
+                Bukkit.getLogger().info(String.format("num: %d", randomNumber));
+            }
+
             player.removeMetadata("EnterNumber", KelsiCasino.getInstance());
-            number = Integer.parseInt(event.getMessage());
-            // random = (max - min)+min
-            if (slot == 1) {
-                if (randomNumber <= 95) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number*1.1));
-                } else {
-                    player.setHealth(0.0);
-                }
-                Bukkit.getLogger().info("number: " + randomNumber);
-            }
-            else if (slot == 2) {
-                if (randomNumber <= 80) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number*1.5));
-                } else {
-                    player.setHealth(0.0);
-                }
-                Bukkit.getLogger().info("number: " + randomNumber);
-            }
-            else if (slot == 3) {
-                if (randomNumber <= 75) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number*1.75));
-                } else {
-                    player.setHealth(0.0);
-                }
-                Bukkit.getLogger().info("number: " + randomNumber);
-            }
-            else if (slot == 4) {
-                if (randomNumber <= 50) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number*2));
-                } else {
-                    player.setHealth(0.0);
-                }
-                Bukkit.getLogger().info("number: " + randomNumber);
-            }
-            else if (slot == 5) {
-                if (randomNumber <= 25) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number*3));
-                } else {
-                    player.setHealth(0.0);
-                }
-                Bukkit.getLogger().info("number: " + randomNumber);
-            }
-            else if (slot == 6) {
-                if (randomNumber <= 10) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number*3.5));
-                } else {
-                    player.setHealth(0.0);
-                }
-                Bukkit.getLogger().info("number: " + randomNumber);
-            }
-            else if (slot == 7) {
-                if (randomNumber <= 1) {
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + player.getName() + " " + Math.round(number*5));
-                } else {
-                    player.setHealth(0.0);
-                }
-                Bukkit.getLogger().info("number: " + randomNumber);
-            }
+            playerSlotNumberMap.remove(player);
         }
     }
 
@@ -90,45 +49,17 @@ public class guiListener implements Listener {
     public void onClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
 
-
         if (player.hasMetadata("OpenedMenu")) {
             event.setCancelled(true);
 
-            if (event.getSlot() == 1) {
-                player.sendMessage("stone");
+            String[] items = {"stone", "iron", "gold", "redstone", "diamond", "diamond3", "diamond5"};
+            if (event.getSlot() >= 1 && event.getSlot() <= 7) {
+                String item = items[event.getSlot() - 1];
+                player.sendMessage(item);
                 player.closeInventory();
                 player.setMetadata("EnterNumber", new FixedMetadataValue(KelsiCasino.getInstance(), "/roll"));
                 player.sendMessage("\nВведите вашу ставку!\n");
-            } else if (event.getSlot() == 2) {
-                player.sendMessage("iron");
-                player.closeInventory();
-                player.setMetadata("EnterNumber", new FixedMetadataValue(KelsiCasino.getInstance(), "/roll"));
-                player.sendMessage("\nВведите вашу ставку!\n");
-            } else if (event.getSlot() == 3) {
-                player.sendMessage("gold");
-                player.closeInventory();
-                player.setMetadata("EnterNumber", new FixedMetadataValue(KelsiCasino.getInstance(), "/roll"));
-                player.sendMessage("\nВведите вашу ставку!\n");
-            } else if (event.getSlot() == 4) {
-                player.sendMessage("redstone");
-                player.closeInventory();
-                player.setMetadata("EnterNumber", new FixedMetadataValue(KelsiCasino.getInstance(), "/roll"));
-                player.sendMessage("\nВведите вашу ставку!\n");
-            } else if (event.getSlot() == 5) {
-                player.sendMessage("diamond");
-                player.closeInventory();
-                player.setMetadata("EnterNumber", new FixedMetadataValue(KelsiCasino.getInstance(), "/roll"));
-                player.sendMessage("\nВведите вашу ставку!\n");
-            } else if (event.getSlot() == 6) {
-                player.sendMessage("diamond3");
-                player.closeInventory();
-                player.setMetadata("EnterNumber", new FixedMetadataValue(KelsiCasino.getInstance(), "/roll"));
-                player.sendMessage("\nВведите вашу ставку!\n");
-            } else if (event.getSlot() == 7) {
-                player.sendMessage("diamond5");
-                player.closeInventory();
-                player.setMetadata("EnterNumber", new FixedMetadataValue(KelsiCasino.getInstance(), "/roll"));
-                player.sendMessage("\nВведите вашу ставку!\n");
+                playerSlotNumberMap.put(player, event.getSlot());
             }
         }
 
@@ -137,7 +68,6 @@ public class guiListener implements Listener {
     @EventHandler
     public void onClose(InventoryCloseEvent event) {
         Player player = (Player) event.getPlayer();
-
         if (player.hasMetadata("OpenedMenu")) {
             player.removeMetadata("OpenedMenu", KelsiCasino.getInstance());
         }
